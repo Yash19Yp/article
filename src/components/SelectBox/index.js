e209446080d140040b69945ec2c35630fe76a459
Yash19Yp <87873408+Yash19Yp@users.noreply.github.com>
2022-07-01 17:12:54 +0530

    Update index.js

--- src/components/SelectBox/index.js
+++ src/components/SelectBox/index.js
@@ -3,9 +3,8 @@ import React from "react";
 import Select from "react-select";
 
 const selectOptions = [
-  { value: "option1", label: "Option1" },
-  { value: "option2", label: "Option2" },
-  { value: "option3", label: "Option3" },
+  { value: "html", label: "html" },
+  { value: "markdown", label: "markdown" },
 ];
 
 export const SelectBox = React.forwardRef(
