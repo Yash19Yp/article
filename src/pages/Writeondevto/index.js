1eedd1110b408766bec86f2c0409236079f230a2
Yash19Yp <87873408+Yash19Yp@users.noreply.github.com>
2022-07-01 17:11:49 +0530

    Update index.js

--- src/pages/Writeondevto/index.js
+++ src/pages/Writeondevto/index.js
@@ -5,12 +5,12 @@ import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import useForm from "simple-react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
-import { Column, Row, Text, Button, Input } from "components";
+import { Column, Row, Text, Button, Input, TextArea } from "components";
 
 const WriteondevtoPage = () => {
   const [apiData1, setapiData1] = React.useState();
   const form = useForm({
-    article: { title: "", body_markdown: "", tags: "", canonical_url: "" },
+    article: { title: "", body_markdown: "", canonical_url: "" },
   });
   const navigate = useNavigate();
 
@@ -19,7 +19,7 @@ const WriteondevtoPage = () => {
     postArticles(req)
       .then((res) => {
         setapiData1(res);
-
+        navigate("/");
         toast.success("Article Posted Successfully.");
       })
       .catch((err) => {
@@ -59,18 +59,20 @@ const WriteondevtoPage = () => {
               name="Group67"
               placeholder={`Title`}
             ></Input>
-            <Input
-              className="bg-transparent border-0 font-medium lg:text-[15px] xl:text-[17px] text-[20px] 3xl:text-[24px] text-bluegray_600 text-left w-[100%]"
-              onChange={(e) => {
-                form.handleChange("article.body_markdown", e.target.value);
-              }}
-              value={form?.values?.article?.body_markdown}
-              name="Tellyourstory"
-              placeholder={""}
-            ></Input>
+            <Column className="lg:mt-[18px] xl:mt-[21px] mt-[24px] 3xl:mt-[28px] w-[100%]">
+              <TextArea
+                className="bg-transparent border border-bluegray_700 border-solid font-medium lg:pb-[535px] xl:pb-[612px] pb-[688px] 3xl:pb-[826px] lg:pl-[24px] xl:pl-[28px] pl-[32px] 3xl:pl-[38px] lg:pr-[575px] xl:pr-[658px] pr-[740px] 3xl:pr-[888px] lg:pt-[24px] xl:pt-[28px] pt-[32px] 3xl:pt-[38px] rounded-radius8 lg:text-[15px] xl:text-[17px] text-[20px] 3xl:text-[24px] text-bluegray_600 text-left w-[100%]"
+                name="Tellyourstory"
+                onChange={(e) => {
+                  form.handleChange("article.body_markdown", e.target.value);
+                }}
+                value={form?.values?.article?.body_markdown}
+                placeholder={"Tell your Story ...."}
+              ></TextArea>
+            </Column>
           </Column>
           <Column className="items-start justify-start lg:mb-[358px] xl:mb-[410px] mb-[461px] 3xl:mb-[553px] lg:ml-[18px] xl:ml-[21px] ml-[24px] 3xl:ml-[28px] w-[31%]">
-            <Text className="font-medium lg:text-[14px] xl:text-[16px] text-[18px] 3xl:text-[21px] text-bluegray_50 text-left w-[auto]">{`Add tags`}</Text>
+            {/* <Text className="font-medium lg:text-[14px] xl:text-[16px] text-[18px] 3xl:text-[21px] text-bluegray_50 text-left w-[auto]">{`Add tags`}</Text>
             <Input
               className="placeholder:bg-transparent bg-transparent border border-bluegray_700 border-solid font-medium lg:mt-[6px] xl:mt-[7px] mt-[8px] 3xl:mt-[9px] lg:pb-[32px] xl:pb-[36px] pb-[41.5px] 2xl:pb-[41px] 3xl:pb-[49px] lg:pl-[18px] xl:pl-[21px] pl-[24px] 3xl:pl-[28px] lg:pt-[23px] xl:pt-[27px] pt-[30.5px] 2xl:pt-[30px] 3xl:pt-[36px] rounded-radius8 lg:text-[15px] xl:text-[17px] text-[20px] 3xl:text-[24px] placeholder:text-bluegray_600 text-bluegray_600 text-left w-[100%]"
               onChange={(e) => {
@@ -79,7 +81,7 @@ const WriteondevtoPage = () => {
               value={form?.values?.article?.tags}
               name="Group71"
               placeholder={`Add tags`}
-            ></Input>
+            ></Input> */}
             <Text className="font-medium lg:mt-[18px] xl:mt-[21px] mt-[24px] 3xl:mt-[28px] lg:text-[14px] xl:text-[16px] text-[18px] 3xl:text-[21px] text-bluegray_50 text-left w-[auto]">{`Canonical link`}</Text>
             <Input
               className="placeholder:bg-transparent bg-transparent border border-bluegray_700 border-solid font-medium lg:mt-[6px] xl:mt-[7px] mt-[8px] 3xl:mt-[9px] lg:pl-[18px] xl:pl-[21px] pl-[24px] 3xl:pl-[28px] lg:py-[14px] xl:py-[16px] py-[18.5px] 2xl:py-[18px] 3xl:py-[22px] rounded-radius8 lg:text-[15px] xl:text-[17px] text-[20px] 3xl:text-[24px] placeholder:text-bluegray_600 text-bluegray_600 text-left w-[100%]"
